17e4775b493a982a59df40ce6524c0efb1383736
Jeremias Pak <jeremiaspak@gmail.com>
2020-01-04 19:05:09 -0300

    fix service not stating on boot

--- install.sh
+++ install.sh
@@ -2,7 +2,9 @@
 echo "Installing wifi reset service to /opt/wifi-reset."
 mkdir -p /opt/wifi-reset
 cp -f wifi-reset.sh /opt/wifi-reset/wifi-reset.sh
-echo "Installing systemd service to run at boot."
+chmod +x /opt/wifi-reset/wifi-reset.sh
+echo "Installing systemd service."
 cp -f wifi-reset.service /lib/systemd/system/wifi-reset.service
-echo "Enabling systemd service."
+chmod 644 /lib/systemd/system/wifi-reset.service
+echo "Enabling systemd service to run at boot"
 systemctl enable wifi-reset.service > /dev/null
--- wifi-reset.sh
+++ wifi-reset.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-# Bring all wifi interfaces down.
+# ip link: https://raspberrypi.stackexchange.com/a/73524
+
 # Identify wifi interfaces as rows from standard output of iwconfig (NOT standard
 # error, those are non-wifi interfaces) which start without whitespace.
-# https://raspberrypi.stackexchange.com/a/73524
 sleep 30
+echo "Bring all wifi interfaces down."
 iwconfig 2> /dev/null | grep -o '^[[:alnum:]]\+' | while read x; do ip link set $x down; done
-# Bring all wifi interfaces up.
+
 sleep 30
+echo "Bring all wifi interfaces up."
 iwconfig 2> /dev/null | grep -o '^[[:alnum:]]\+' | while read x; do ip link set $x up; done
